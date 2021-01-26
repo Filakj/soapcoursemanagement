900017b4b5a51ad1c83fc2bae5220b36d9ca71b9
Filakj <59075161+Filakj@users.noreply.github.com>
2021-01-26 17:02:25 -0500

    initialize spring and boot with dependencies

---
+++ src/main/java/com/soapinspring/soapcoursemanagement/SoapcoursemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.soapinspring.soapcoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapcoursemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapcoursemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soapinspring/soapcoursemanagement/SoapcoursemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soapinspring.soapcoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapcoursemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
